4161639d9284aed54f7ade948b116b03de564016
unknown <169665@newtouch.nt>
2020-07-24 17:29:35 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,154 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <el-container>
+    <el-header>
+      <div style="margin-top: 0px">
+        <h1 style="display: block; margin: 0; float: left; ">
+          <a href="/"><img src="./assets/logo.png" style="width: 40px; height: 40px; margin-top: -10px"></a>
+        </h1>
+
+        <ul style="list-style:none; ">
+          <li class="m_li" style="margin-left: 40px">
+            <router-link to="/">Home</router-link>
+          </li>
+          <li class="m_li">
+            <router-link to="/echarts">Echarts</router-link>
+          </li>
+          <li class="m_li">
+            <router-link to="/notes">Notes</router-link>
+          </li>
+          <li class="m_li" :title="themeTitle">
+            <el-dropdown trigger="click" :hide-on-click="false">
+              <span class="el-dropdown-link">
+                Theme<i class="el-icon-arrow-down el-icon--right"></i>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>
+                  <el-switch @change="switchChangeBgc"
+                    v-model="isDark"
+                    active-text="暗夜"
+                    inactive-text="日间"
+                    active-color="#A9A9A9">
+                  </el-switch>
+                </el-dropdown-item>
+                <el-dropdown-item>
+                  <div class="block">
+                    <span class="demonstration">自定义</span>
+                    <el-color-picker @change="pickerChangeBgc" @active-change="pickerChangeBgc" v-model="pickerColor" size="small" style="float: right;"></el-color-picker>
+                  </div>
+                </el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </li>
+          <li class="m_li" :title="aboutTitle">
+            <el-link disabled>About</el-link>
+          </li>
+          <li class="m_li" style="float: right">
+            <el-input
+              placeholder="搜索本站"
+              prefix-icon="el-icon-search"
+              v-model="input"
+              clearable>
+            </el-input>
+          </li>
+        </ul>
+      </div>
+    </el-header>
+
+    <el-main height="2000px"> 
+        <router-view></router-view>
+    </el-main>
+  </el-container>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        input: '',
+        isDark: false,
+        pickerColor: '#ffffff',
+        themeTitle: '更改主体颜色',
+        aboutTitle: '关于我'
+      }
+    },
+    methods: {
+      switchChangeBgc(val) {
+        console.log(val);
+        if (val) {
+          document.body.style.backgroundColor = "#A9A9A9",
+          this.pickerColor = "#A9A9A9"
+        } else {
+          document.body.style.backgroundColor = "#ffffff",
+          this.pickerColor = "#ffffff"
+        } 
+      },
+      pickerChangeBgc(val) {
+        console.log(val);
+        document.body.style.backgroundColor = val;
+      }
+    },
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  .el-header {
+    /*background-color: #B3C0D1;*/
+    color: #333;
+    text-align: center;
+    height: 80px;
+    line-height: 20px;
+  }
+  
+  .el-main {
+    /*background-color: #E9EEF3;*/
+    color: #333;
+    line-height: 30px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
 
-#nav {
-  padding: 30px;
-}
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  .el-dropdown-link {
+    cursor: pointer;
+    color: #409EFF;
+  }
+  .el-icon-arrow-down {
+    font-size: 12px;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .m_li {
+    float: left;
+    margin-left: 30px
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .router-link-active {
+    text-decoration: none;
+  }
+
+  /* 在全局CSS里引用: */
+  /* 滚动条宽度 */
+  ::-webkit-scrollbar {
+   width: 7px;
+   height: 10px;
+  }
+  /* 滚动条的滑块 */
+  ::-webkit-scrollbar-thumb {
+   background-color: #a1a3a9;
+   border-radius: 3px;
+  }
 </style>
---
+++ src/views/Echarts.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="Echarts" style="margin-top: 50px">
+    <div id="histogram" style="width: 49%; height:400px; float: left"></div>
+    <div id="pieChart" style="width: 49%; height:400px; float: left"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Echarts',
+  methods:{
+	  histograms(){
+		  // 基于准备好的dom，初始化echarts实例
+		  var myChart = this.$echarts.init(document.getElementById('histogram'));
+
+		  // 指定图表的配置项和数据
+		  var option = {
+			  title: {
+				  text: '柱状图',
+				  left: 'center'
+			  },
+			  tooltip: {
+			  	show: true,
+			    feature: {
+			      dataZoom: {
+			        yAxisIndex: "none"
+			      },
+			      dataView: {
+			        readOnly: false
+			      },
+			      magicType: {
+			        type: ["line", "bar"]
+			      },
+			      restore: {},
+			      saveAsImage: {}
+			  	}
+			  },
+			  legend: {
+				  data:['销量','价格'],
+				  left: 'right'
+			  },
+			  xAxis: {
+				  data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+			  },
+			  yAxis: {},
+			  series: [{
+				  name: '销量',
+				  type: 'bar',
+				  data: [5, 20, 36, 10, 10, 20]			
+			  },
+			  {
+				  name: '价格',
+				  type: 'bar',
+				  data: [12, 30, 10, 19, 19, 9]
+			  }]
+		  };
+
+		  // 使用刚指定的配置项和数据显示图表。
+		  myChart.setOption(option);
+		  },
+
+		pieCharts(){
+		  // 基于准备好的dom，初始化echarts实例
+		  var myChart = this.$echarts.init(document.getElementById('pieChart'));
+
+		  // 指定图表的配置项和数据
+		  var option = {
+			    title: {
+			        text: '饼状图',
+			        subtext: '纯属虚构',
+			        left: 'center'
+			    },
+			    tooltip: {
+			        trigger: 'item',
+			        formatter: '{a} <br/>{b} : {c} ({d}%)'
+			    },
+			    legend: {
+			        orient: 'vertical',
+			        left: 'right',
+			        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+			    },
+			    series: [
+			        {
+			            name: '访问来源',
+			            type: 'pie',
+			            radius: '55%',
+			            center: ['50%', '60%'],
+			            data: [
+			                {value: 335, name: '直接访问'},
+			                {value: 310, name: '邮件营销'},
+			                {value: 234, name: '联盟广告'},
+			                {value: 135, name: '视频广告'},
+			                {value: 1548, name: '搜索引擎'}
+			            ],
+			            emphasis: {
+			                itemStyle: {
+			                    shadowBlur: 10,
+			                    shadowOffsetX: 0,
+			                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+			                }
+			            }
+			        }
+			    ]
+			};
+		  // 使用刚指定的配置项和数据显示图表。
+		  myChart.setOption(option);
+		  }
+  },
+  mounted() {
+  	this.histograms();
+  	this.pieCharts();
+  }
+}
+</script>
